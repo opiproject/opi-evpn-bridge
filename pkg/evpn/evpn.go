19e5bfac0a8565e25d5f289431df8903f7111a3e
Boris Glimcher <Boris.Glimcher@emc.com>
2023-05-12 00:27:11 +0300

    interface: add create and delete methods
    Signed-off-by: Boris Glimcher <Boris.Glimcher@emc.com>

--- pkg/evpn/evpn.go
+++ pkg/evpn/evpn.go
@@ -20,7 +20,8 @@ import (
 // Server represents the Server object
 type Server struct {
 	pb.UnimplementedCloudInfraServiceServer
-	Subnets map[string]*pb.Subnet
+	Subnets    map[string]*pb.Subnet
+	Interfaces map[string]*pb.Interface
 }
 
 // CreateSubnet executes the creation of the subnet
@@ -101,5 +102,76 @@ func (s *Server) DeleteSubnet(_ context.Context, in *pb.DeleteSubnetRequest) (*e
 	}
 
 	delete(s.Subnets, snet.Spec.Id.Value)
+	return &emptypb.Empty{}, nil
+}
+
+// CreateInterface executes the creation of the interface
+func (s *Server) CreateInterface(_ context.Context, in *pb.CreateInterfaceRequest) (*pb.Interface, error) {
+	log.Printf("CreateInterface: Received from client: %v", in)
+	// idempotent API when called with same key, should return same object
+	iface, ok := s.Interfaces[in.Interface.Spec.Id.Value]
+	if ok {
+		log.Printf("Already existing Interface with id %v", in.Interface.Spec.Id.Value)
+		return iface, nil
+	}
+	// not found, so create a new one
+	snet, ok := s.Subnets[in.Interface.Spec.Id.Value]
+	if !ok {
+		// TODO: change Spec.Id.Value to bridge reference instead
+		err := status.Errorf(codes.NotFound, "unable to find key %s", in.Interface.Spec.Id.Value)
+		log.Printf("error: %v", err)
+		return nil, err
+	}
+	// Get an existing network bridge
+	br, err := tenus.BridgeFromName(snet.Spec.Id.Value)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Create a dummy link
+	dl, err := tenus.NewLink("mydummylink")
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Add the dummy link into bridge
+	if err = br.AddSlaveIfc(dl.NetInterface()); err != nil {
+		log.Fatal(err)
+	}
+
+	// Bring the dummy link up
+	if err = dl.SetLinkUp(); err != nil {
+		fmt.Println(err)
+	}
+	// TODO: replace cloud -> evpn
+	s.Interfaces[in.Interface.Spec.Id.Value] = in.Interface
+	response := &pb.Interface{}
+	err = deepcopier.Copy(in.Interface).To(response)
+	if err != nil {
+		log.Printf("error: %v", err)
+		return nil, err
+	}
+	response.Status = &pb.InterfaceStatus{IfIndex: 8}
+	return response, nil
+}
+
+// DeleteSubnet deletes an interface
+func (s *Server) DeleteInterface(_ context.Context, in *pb.DeleteInterfaceRequest) (*emptypb.Empty, error) {
+	log.Printf("DeleteInterface: Received from client: %v", in)
+	iface, ok := s.Interfaces[in.Id]
+	if !ok {
+		if in.AllowMissing {
+			return &emptypb.Empty{}, nil
+		}
+		err := status.Errorf(codes.NotFound, "unable to find key %s", in.Id)
+		log.Printf("error: %v", err)
+		return nil, err
+	}
+
+	// Delete link
+	// $ sudo ip link delete br0 type bridge
+	if err := tenus.DeleteLink(iface.Spec.Id.Value); err != nil {
+		log.Fatal(err)
+	}
+
+	delete(s.Interfaces, iface.Spec.Id.Value)
 	return &emptypb.Empty{}, nil
 }
