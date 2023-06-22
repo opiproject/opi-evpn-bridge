c27a8f4e010da055cdfeb7f04acfd15eb01e8a45
Boris Glimcher <Boris.Glimcher@emc.com>
2023-06-22 17:34:00 +0300

    tests: add VPC unittests
    Signed-off-by: Boris Glimcher <Boris.Glimcher@emc.com>

--- pkg/evpn/evpn_test.go
+++ pkg/evpn/evpn_test.go
@@ -3,3 +3,32 @@
 
 // Package evpn is the main package of the application
 package evpn
+
+import (
+	"context"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/test/bufconn"
+
+	pb "github.com/opiproject/opi-api/network/cloud/v1alpha1/gen/go"
+)
+
+// TODO: move test infrastructure code to a separate (test/server) package to avoid duplication
+
+func dialer() func(context.Context, string) (net.Conn, error) {
+	listener := bufconn.Listen(1024 * 1024)
+	server := grpc.NewServer()
+	pb.RegisterCloudInfraServiceServer(server, &Server{})
+
+	go func() {
+		if err := server.Serve(listener); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	return func(context.Context, string) (net.Conn, error) {
+		return listener.Dial()
+	}
+}
---
+++ pkg/evpn/vrf_vpc_test.go
@@ -0,0 +1,265 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (c) 2022-2023 Intel Corporation, or its subsidiaries.
+
+// Package evpn is the main package of the application
+package evpn
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"log"
+	"reflect"
+	"testing"
+
+	"google.golang.org/protobuf/proto"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/emptypb"
+
+	pb "github.com/opiproject/opi-api/network/cloud/v1alpha1/gen/go"
+)
+
+func resourceIDToVolumeName(resourceID string) string {
+	return fmt.Sprintf("//network.opiproject.org/vpcs/%s", resourceID)
+}
+
+var (
+	testVpcID   = "opi-vpc8"
+	testVpcName = resourceIDToVolumeName(testVpcID)
+	testVpc     = pb.Vpc{
+		Spec: &pb.VpcSpec{
+			V4RouteTableNameRef: "1000",
+		},
+	}
+)
+
+func Test_CreateVpc(t *testing.T) {
+	tests := map[string]struct {
+		id      string
+		in      *pb.Vpc
+		out     *pb.Vpc
+		errCode codes.Code
+		errMsg  string
+		exist   bool
+	}{
+		"illegal resource_id": {
+			"CapitalLettersNotAllowed",
+			&testVpc,
+			nil,
+			codes.Unknown,
+			fmt.Sprintf("user-settable ID must only contain lowercase, numbers and hyphens (%v)", "got: 'C' in position 0"),
+			false,
+		},
+		// "already exists": {
+		// 	testVpcID,
+		// 	&testVpc,
+		// 	&testVpc,
+		// 	codes.OK,
+		// 	"",
+		// 	true,
+		// },
+	}
+
+	// run tests
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			// start GRPC mockup server
+			ctx := context.Background()
+			conn, err := grpc.DialContext(ctx, "", grpc.WithTransportCredentials(insecure.NewCredentials()), grpc.WithContextDialer(dialer()))
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer func(conn *grpc.ClientConn) {
+				err := conn.Close()
+				if err != nil {
+					log.Fatal(err)
+				}
+			}(conn)
+			client := pb.NewCloudInfraServiceClient(conn)
+
+			if tt.exist {
+				// s.Vpcs[testVpcName] = &testVpc
+			}
+			if tt.out != nil {
+				tt.out.Name = testVpcName
+			}
+
+			request := &pb.CreateVpcRequest{Vpc: tt.in, VpcId: tt.id, Parent: "todo"}
+			response, err := client.CreateVpc(ctx, request)
+			if response != nil {
+				// if !reflect.DeepEqual(response, tt.out) {
+				mtt, _ := proto.Marshal(tt.out)
+				mResponse, _ := proto.Marshal(response)
+				if !bytes.Equal(mtt, mResponse) {
+					t.Error("response: expected", tt.out, "received", response)
+				}
+			}
+
+			if err != nil {
+				if er, ok := status.FromError(err); ok {
+					if er.Code() != tt.errCode {
+						t.Error("error code: expected", tt.errCode, "received", er.Code())
+					}
+					if er.Message() != tt.errMsg {
+						t.Error("error message: expected", tt.errMsg, "received", er.Message())
+					}
+				}
+			}
+		})
+	}
+}
+
+func Test_DeleteVpc(t *testing.T) {
+	tests := map[string]struct {
+		in      string
+		out     *emptypb.Empty
+		errCode codes.Code
+		errMsg  string
+		missing bool
+	}{
+		// "valid request": {
+		// 	testVpcID,
+		// 	&emptypb.Empty{},
+		// 	codes.OK,
+		// 	"",
+		// 	false,
+		// },
+		"valid request with unknown key": {
+			"unknown-id",
+			nil,
+			codes.NotFound,
+			fmt.Sprintf("unable to find key %v", resourceIDToVolumeName("unknown-id")),
+			false,
+		},
+		"unknown key with missing allowed": {
+			"unknown-id",
+			&emptypb.Empty{},
+			codes.OK,
+			"",
+			true,
+		},
+		"malformed name": {
+			"-ABC-DEF",
+			&emptypb.Empty{},
+			codes.Unknown,
+			fmt.Sprintf("segment '%s': not a valid DNS name", "-ABC-DEF"),
+			false,
+		},
+	}
+
+	// run tests
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			// start GRPC mockup server
+			ctx := context.Background()
+			conn, err := grpc.DialContext(ctx, "", grpc.WithTransportCredentials(insecure.NewCredentials()), grpc.WithContextDialer(dialer()))
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer func(conn *grpc.ClientConn) {
+				err := conn.Close()
+				if err != nil {
+					log.Fatal(err)
+				}
+			}(conn)
+			client := pb.NewCloudInfraServiceClient(conn)
+
+			fname1 := resourceIDToVolumeName(tt.in)
+			// s.Vpcs[testVpcName] = &testVpc
+
+			request := &pb.DeleteVpcRequest{Name: fname1, AllowMissing: tt.missing}
+			response, err := client.DeleteVpc(ctx, request)
+			if err != nil {
+				if er, ok := status.FromError(err); ok {
+					if er.Code() != tt.errCode {
+						t.Error("error code: expected", tt.errCode, "received", er.Code())
+					}
+					if er.Message() != tt.errMsg {
+						t.Error("error message: expected", tt.errMsg, "received", er.Message())
+					}
+				}
+			}
+			if reflect.TypeOf(response) != reflect.TypeOf(tt.out) {
+				t.Error("response: expected", reflect.TypeOf(tt.out), "received", reflect.TypeOf(response))
+			}
+		})
+	}
+}
+
+func Test_GetVpc(t *testing.T) {
+	tests := map[string]struct {
+		in      string
+		out     *pb.Vpc
+		errCode codes.Code
+		errMsg  string
+	}{
+		// "valid request": {
+		// 	testVpcID,
+		// 	&pb.Vpc{
+		// 		Name:      testVpcName,
+		// 		Multipath: testVpc.Multipath,
+		// 	},
+		// 	codes.OK,
+		// 	"",
+		// },
+		"valid request with unknown key": {
+			"unknown-id",
+			nil,
+			codes.NotFound,
+			fmt.Sprintf("unable to find key %v", "unknown-id"),
+		},
+		"malformed name": {
+			"-ABC-DEF",
+			nil,
+			codes.Unknown,
+			fmt.Sprintf("segment '%s': not a valid DNS name", "-ABC-DEF"),
+		},
+	}
+
+	// run tests
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			// start GRPC mockup server
+			ctx := context.Background()
+			conn, err := grpc.DialContext(ctx, "", grpc.WithTransportCredentials(insecure.NewCredentials()), grpc.WithContextDialer(dialer()))
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer func(conn *grpc.ClientConn) {
+				err := conn.Close()
+				if err != nil {
+					log.Fatal(err)
+				}
+			}(conn)
+			client := pb.NewCloudInfraServiceClient(conn)
+
+			// s.Vpcs[testVpcID] = &testVpc
+
+			request := &pb.GetVpcRequest{Name: tt.in}
+			response, err := client.GetVpc(ctx, request)
+			if response != nil {
+				// if !reflect.DeepEqual(response, tt.out) {
+				mtt, _ := proto.Marshal(tt.out)
+				mResponse, _ := proto.Marshal(response)
+				if !bytes.Equal(mtt, mResponse) {
+					t.Error("response: expected", tt.out, "received", response)
+				}
+			}
+
+			if err != nil {
+				if er, ok := status.FromError(err); ok {
+					if er.Code() != tt.errCode {
+						t.Error("error code: expected", tt.errCode, "received", er.Code())
+					}
+					if er.Message() != tt.errMsg {
+						t.Error("error message: expected", tt.errMsg, "received", er.Message())
+					}
+				}
+			}
+		})
+	}
+}
