adcf1bcf2da869c70a99d57ce99ad544011da936
Boris Glimcher <Boris.Glimcher@emc.com>
2023-06-22 22:20:01 +0300

    tests: add INTERFACE unittests
    Signed-off-by: Boris Glimcher <Boris.Glimcher@emc.com>

---
+++ pkg/evpn/interface_test.go
@@ -0,0 +1,273 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (c) 2022-2023 Intel Corporation, or its subsidiaries.
+// Copyright (c) 2022-2023 Dell Inc, or its subsidiaries.
+
+// Package evpn is the main package of the application
+package evpn
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"log"
+	"reflect"
+	"testing"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/types/known/emptypb"
+
+	pb "github.com/opiproject/opi-api/network/cloud/v1alpha1/gen/go"
+)
+
+var (
+	testInterfaceID   = "opi-interface8"
+	testInterfaceName = resourceIDToVolumeName("interfaces", testInterfaceID)
+	testInterface     = pb.Interface{
+		Spec: &pb.InterfaceSpec{
+			Ifid: 11,
+		},
+	}
+)
+
+func Test_CreateInterface(t *testing.T) {
+	tests := map[string]struct {
+		id      string
+		in      *pb.Interface
+		out     *pb.Interface
+		errCode codes.Code
+		errMsg  string
+		exist   bool
+	}{
+		"illegal resource_id": {
+			"CapitalLettersNotAllowed",
+			&testInterface,
+			nil,
+			codes.Unknown,
+			fmt.Sprintf("user-settable ID must only contain lowercase, numbers and hyphens (%v)", "got: 'C' in position 0"),
+			false,
+		},
+		"already exists": {
+			testInterfaceID,
+			&testInterface,
+			&testInterface,
+			codes.OK,
+			"",
+			true,
+		},
+	}
+
+	// run tests
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			// start GRPC mockup server
+			ctx := context.Background()
+			opi := NewServer()
+			conn, err := grpc.DialContext(ctx,
+				"",
+				grpc.WithTransportCredentials(insecure.NewCredentials()),
+				grpc.WithContextDialer(dialer(opi)))
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer func(conn *grpc.ClientConn) {
+				err := conn.Close()
+				if err != nil {
+					log.Fatal(err)
+				}
+			}(conn)
+			client := pb.NewCloudInfraServiceClient(conn)
+
+			if tt.exist {
+				opi.Interfaces[testInterfaceName] = &testInterface
+			}
+			if tt.out != nil {
+				tt.out.Name = testInterfaceName
+			}
+
+			request := &pb.CreateInterfaceRequest{Interface: tt.in, InterfaceId: tt.id, Parent: "todo"}
+			response, err := client.CreateInterface(ctx, request)
+			if response != nil {
+				// if !reflect.DeepEqual(response, tt.out) {
+				mtt, _ := proto.Marshal(tt.out)
+				mResponse, _ := proto.Marshal(response)
+				if !bytes.Equal(mtt, mResponse) {
+					t.Error("response: expected", tt.out, "received", response)
+				}
+			}
+
+			if err != nil {
+				if er, ok := status.FromError(err); ok {
+					if er.Code() != tt.errCode {
+						t.Error("error code: expected", tt.errCode, "received", er.Code())
+					}
+					if er.Message() != tt.errMsg {
+						t.Error("error message: expected", tt.errMsg, "received", er.Message())
+					}
+				}
+			}
+		})
+	}
+}
+
+func Test_DeleteInterface(t *testing.T) {
+	tests := map[string]struct {
+		in      string
+		out     *emptypb.Empty
+		errCode codes.Code
+		errMsg  string
+		missing bool
+	}{
+		// "valid request": {
+		// 	testInterfaceID,
+		// 	&emptypb.Empty{},
+		// 	codes.OK,
+		// 	"",
+		// 	false,
+		// },
+		"valid request with unknown key": {
+			"unknown-id",
+			nil,
+			codes.NotFound,
+			fmt.Sprintf("unable to find key %v", resourceIDToVolumeName("interfaces", "unknown-id")),
+			false,
+		},
+		"unknown key with missing allowed": {
+			"unknown-id",
+			&emptypb.Empty{},
+			codes.OK,
+			"",
+			true,
+		},
+		"malformed name": {
+			"-ABC-DEF",
+			&emptypb.Empty{},
+			codes.Unknown,
+			fmt.Sprintf("segment '%s': not a valid DNS name", "-ABC-DEF"),
+			false,
+		},
+	}
+
+	// run tests
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			// start GRPC mockup server
+			ctx := context.Background()
+			opi := NewServer()
+			conn, err := grpc.DialContext(ctx,
+				"",
+				grpc.WithTransportCredentials(insecure.NewCredentials()),
+				grpc.WithContextDialer(dialer(opi)))
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer func(conn *grpc.ClientConn) {
+				err := conn.Close()
+				if err != nil {
+					log.Fatal(err)
+				}
+			}(conn)
+			client := pb.NewCloudInfraServiceClient(conn)
+
+			fname1 := resourceIDToVolumeName("interfaces", tt.in)
+			opi.Interfaces[testInterfaceName] = &testInterface
+
+			request := &pb.DeleteInterfaceRequest{Name: fname1, AllowMissing: tt.missing}
+			response, err := client.DeleteInterface(ctx, request)
+			if err != nil {
+				if er, ok := status.FromError(err); ok {
+					if er.Code() != tt.errCode {
+						t.Error("error code: expected", tt.errCode, "received", er.Code())
+					}
+					if er.Message() != tt.errMsg {
+						t.Error("error message: expected", tt.errMsg, "received", er.Message())
+					}
+				}
+			}
+			if reflect.TypeOf(response) != reflect.TypeOf(tt.out) {
+				t.Error("response: expected", reflect.TypeOf(tt.out), "received", reflect.TypeOf(response))
+			}
+		})
+	}
+}
+
+func Test_GetInterface(t *testing.T) {
+	tests := map[string]struct {
+		in      string
+		out     *pb.Interface
+		errCode codes.Code
+		errMsg  string
+	}{
+		// "valid request": {
+		// 	testInterfaceID,
+		// 	&pb.Interface{
+		// 		Name:      testInterfaceName,
+		// 		Multipath: testInterface.Multipath,
+		// 	},
+		// 	codes.OK,
+		// 	"",
+		// },
+		"valid request with unknown key": {
+			"unknown-id",
+			nil,
+			codes.NotFound,
+			fmt.Sprintf("unable to find key %v", "unknown-id"),
+		},
+		"malformed name": {
+			"-ABC-DEF",
+			nil,
+			codes.Unknown,
+			fmt.Sprintf("segment '%s': not a valid DNS name", "-ABC-DEF"),
+		},
+	}
+
+	// run tests
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			// start GRPC mockup server
+			ctx := context.Background()
+			opi := NewServer()
+			conn, err := grpc.DialContext(ctx,
+				"",
+				grpc.WithTransportCredentials(insecure.NewCredentials()),
+				grpc.WithContextDialer(dialer(opi)))
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer func(conn *grpc.ClientConn) {
+				err := conn.Close()
+				if err != nil {
+					log.Fatal(err)
+				}
+			}(conn)
+			client := pb.NewCloudInfraServiceClient(conn)
+
+			opi.Interfaces[testInterfaceID] = &testInterface
+
+			request := &pb.GetInterfaceRequest{Name: tt.in}
+			response, err := client.GetInterface(ctx, request)
+			if response != nil {
+				// if !reflect.DeepEqual(response, tt.out) {
+				mtt, _ := proto.Marshal(tt.out)
+				mResponse, _ := proto.Marshal(response)
+				if !bytes.Equal(mtt, mResponse) {
+					t.Error("response: expected", tt.out, "received", response)
+				}
+			}
+
+			if err != nil {
+				if er, ok := status.FromError(err); ok {
+					if er.Code() != tt.errCode {
+						t.Error("error code: expected", tt.errCode, "received", er.Code())
+					}
+					if er.Message() != tt.errMsg {
+						t.Error("error message: expected", tt.errMsg, "received", er.Message())
+					}
+				}
+			}
+		})
+	}
+}
