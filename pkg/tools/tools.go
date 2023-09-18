bda891760f3d6ecf56e29cd42f53c743ffca97aa
Boris Glimcher <36732377+glimchb@users.noreply.github.com>
2023-09-18 15:48:07 -0400

    chore: update mockery to v2
    Signed-off-by: Boris Glimcher <36732377+glimchb@users.noreply.github.com>

--- pkg/tools/tools.go
+++ pkg/tools/tools.go
@@ -7,6 +7,6 @@ package tools
 import (
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
 	_ "github.com/onsi/ginkgo/v2/ginkgo"
-	_ "github.com/vektra/mockery/cmd/mockery"
+	_ "github.com/vektra/mockery/v2/cmd"
 	_ "golang.org/x/tools/cmd/goimports"
 )
