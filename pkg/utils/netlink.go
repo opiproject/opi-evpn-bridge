b7597ca447bd24b83ab42f5ef7406d9c2093097c
Boris Glimcher <Boris.Glimcher@emc.com>
2023-10-06 19:15:09 +0300

    feat(trace): add nested traces to netlink
    Signed-off-by: Boris Glimcher <Boris.Glimcher@emc.com>

--- pkg/utils/netlink.go
+++ pkg/utils/netlink.go
@@ -9,6 +9,10 @@ import (
 	"net"
 
 	"github.com/vishvananda/netlink"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/trace"
 )
 
 // Netlink represents limited subset of functions from netlink package
@@ -30,72 +34,105 @@ type Netlink interface {
 
 // NetlinkWrapper wrapper for netlink package
 type NetlinkWrapper struct {
+	tracer trace.Tracer
+}
+
+// NewNetlinkWrapper creates initialized instance of NetlinkWrapper
+func NewNetlinkWrapper() *NetlinkWrapper {
+	return &NetlinkWrapper{tracer: otel.Tracer("")}
 }
 
 // build time check that struct implements interface
 var _ Netlink = (*NetlinkWrapper)(nil)
 
 // LinkByName is a wrapper for netlink.LinkByName
-func (n *NetlinkWrapper) LinkByName(_ context.Context, name string) (netlink.Link, error) {
+func (n *NetlinkWrapper) LinkByName(ctx context.Context, name string) (netlink.Link, error) {
+	_, childSpan := n.tracer.Start(ctx, "netlink.LinkByName")
+	childSpan.SetAttributes(attribute.String("link.name", name))
+	defer childSpan.End()
 	return netlink.LinkByName(name)
 }
 
 // LinkModify is a wrapper for netlink.LinkModify
-func (n *NetlinkWrapper) LinkModify(_ context.Context, link netlink.Link) error {
+func (n *NetlinkWrapper) LinkModify(ctx context.Context, link netlink.Link) error {
+	_, childSpan := n.tracer.Start(ctx, "netlink.LinkModify")
+	defer childSpan.End()
 	return netlink.LinkModify(link)
 }
 
 // LinkSetHardwareAddr is a wrapper for netlink.LinkSetHardwareAddr
-func (n *NetlinkWrapper) LinkSetHardwareAddr(_ context.Context, link netlink.Link, hwaddr net.HardwareAddr) error {
+func (n *NetlinkWrapper) LinkSetHardwareAddr(ctx context.Context, link netlink.Link, hwaddr net.HardwareAddr) error {
+	_, childSpan := n.tracer.Start(ctx, "netlink.LinkSetHardwareAddr")
+	defer childSpan.End()
 	return netlink.LinkSetHardwareAddr(link, hwaddr)
 }
 
 // AddrAdd is a wrapper for netlink.AddrAdd
-func (n *NetlinkWrapper) AddrAdd(_ context.Context, link netlink.Link, addr *netlink.Addr) error {
+func (n *NetlinkWrapper) AddrAdd(ctx context.Context, link netlink.Link, addr *netlink.Addr) error {
+	_, childSpan := n.tracer.Start(ctx, "netlink.AddrAdd")
+	defer childSpan.End()
 	return netlink.AddrAdd(link, addr)
 }
 
 // AddrDel is a wrapper for netlink.AddrDel
-func (n *NetlinkWrapper) AddrDel(_ context.Context, link netlink.Link, addr *netlink.Addr) error {
+func (n *NetlinkWrapper) AddrDel(ctx context.Context, link netlink.Link, addr *netlink.Addr) error {
+	_, childSpan := n.tracer.Start(ctx, "netlink.AddrDel")
+	defer childSpan.End()
 	return netlink.AddrDel(link, addr)
 }
 
 // LinkAdd is a wrapper for netlink.LinkAdd
-func (n *NetlinkWrapper) LinkAdd(_ context.Context, link netlink.Link) error {
+func (n *NetlinkWrapper) LinkAdd(ctx context.Context, link netlink.Link) error {
+	_, childSpan := n.tracer.Start(ctx, "netlink.LinkAdd")
+	defer childSpan.End()
 	return netlink.LinkAdd(link)
 }
 
 // LinkDel is a wrapper for netlink.LinkDel
-func (n *NetlinkWrapper) LinkDel(_ context.Context, link netlink.Link) error {
+func (n *NetlinkWrapper) LinkDel(ctx context.Context, link netlink.Link) error {
+	_, childSpan := n.tracer.Start(ctx, "netlink.LinkDel")
+	defer childSpan.End()
 	return netlink.LinkDel(link)
 }
 
 // LinkSetUp is a wrapper for netlink.LinkSetUp
-func (n *NetlinkWrapper) LinkSetUp(_ context.Context, link netlink.Link) error {
+func (n *NetlinkWrapper) LinkSetUp(ctx context.Context, link netlink.Link) error {
+	_, childSpan := n.tracer.Start(ctx, "netlink.LinkSetUp")
+	defer childSpan.End()
 	return netlink.LinkSetUp(link)
 }
 
 // LinkSetDown is a wrapper for netlink.LinkSetDown
-func (n *NetlinkWrapper) LinkSetDown(_ context.Context, link netlink.Link) error {
+func (n *NetlinkWrapper) LinkSetDown(ctx context.Context, link netlink.Link) error {
+	_, childSpan := n.tracer.Start(ctx, "netlink.LinkSetDown")
+	defer childSpan.End()
 	return netlink.LinkSetDown(link)
 }
 
 // LinkSetMaster is a wrapper for netlink.LinkSetMaster
-func (n *NetlinkWrapper) LinkSetMaster(_ context.Context, link, master netlink.Link) error {
+func (n *NetlinkWrapper) LinkSetMaster(ctx context.Context, link, master netlink.Link) error {
+	_, childSpan := n.tracer.Start(ctx, "netlink.LinkSetMaster")
+	defer childSpan.End()
 	return netlink.LinkSetMaster(link, master)
 }
 
 // LinkSetNoMaster is a wrapper for netlink.LinkSetNoMaster
-func (n *NetlinkWrapper) LinkSetNoMaster(_ context.Context, link netlink.Link) error {
+func (n *NetlinkWrapper) LinkSetNoMaster(ctx context.Context, link netlink.Link) error {
+	_, childSpan := n.tracer.Start(ctx, "netlink.LinkSetNoMaster")
+	defer childSpan.End()
 	return netlink.LinkSetNoMaster(link)
 }
 
 // BridgeVlanAdd is a wrapper for netlink.BridgeVlanAdd
-func (n *NetlinkWrapper) BridgeVlanAdd(_ context.Context, link netlink.Link, vid uint16, pvid, untagged, self, master bool) error {
+func (n *NetlinkWrapper) BridgeVlanAdd(ctx context.Context, link netlink.Link, vid uint16, pvid, untagged, self, master bool) error {
+	_, childSpan := n.tracer.Start(ctx, "netlink.BridgeVlanAdd")
+	defer childSpan.End()
 	return netlink.BridgeVlanAdd(link, vid, pvid, untagged, self, master)
 }
 
 // BridgeVlanDel is a wrapper for netlink.BridgeVlanDel
-func (n *NetlinkWrapper) BridgeVlanDel(_ context.Context, link netlink.Link, vid uint16, pvid, untagged, self, master bool) error {
+func (n *NetlinkWrapper) BridgeVlanDel(ctx context.Context, link netlink.Link, vid uint16, pvid, untagged, self, master bool) error {
+	_, childSpan := n.tracer.Start(ctx, "netlink.BridgeVlanDel")
+	defer childSpan.End()
 	return netlink.BridgeVlanDel(link, vid, pvid, untagged, self, master)
 }
