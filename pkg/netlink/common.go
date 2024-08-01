0596327a7679b4f706abdcb72ecbd6514522a646
Saikumar, Banoth <banoth.saikumar@intel.com>
2024-08-01 08:27:45 +0100

    fix(evpn): netlink split changes
    Signed-off-by: Saikumar, Banoth <banoth.saikumar@intel.com>

--- pkg/netlink/common.go
+++ pkg/netlink/common.go
@@ -20,8 +20,8 @@ var ctx context.Context
 
 var nlink utils.Netlink
 
-// eventBus variable
-var eventBus = eb.NewEventBus()
+// EventBus variable
+var EventBus = eb.NewEventBus()
 
 // pollInterval variable
 var pollInterval int
@@ -270,5 +270,5 @@ func deepCheck(v1 interface{}, v2 interface{}) bool {
 
 func notifyAddDel(r interface{}, event string) {
 	log.Printf("netlink: Notify event: %s\n", event)
-	eventBus.Publish(event, r)
+	EventBus.Publish(event, r)
 }
