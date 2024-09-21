2269e647b392b92fbbaa5290759d3a5c740b5ada
Vemula Venkatesh <venkatesh.vemula@intel.com>
2024-09-21 20:02:38 +0100

    feat(IDPool): IDPool feature for generating ID's

--- pkg/utils/Idpool.go
+++ pkg/utils/Idpool.go
@@ -12,7 +12,8 @@ import (
 	// "github.com/opiproject/opi-evpn-bridge/pkg/vendor_plugins/intel-e2000/p4runtime/p4translation"
 )
 
-/*  IDPool Helper class for uniquely assigning IDs from a specified integer set (e.g. a
+// IDPool structure
+/*  Helper class for uniquely assigning IDs from a specified integer set (e.g. a
 #   range) to keys. IDs are assigned (or read) with get_id(key) and returned back
 #   into the pool with release_id(key). The IDPool remembers a once-assigned ID
 #   for keys so that the same ID is assigned for a key. Only when the pool runs
@@ -63,7 +64,7 @@ func (ip *IDPool) _assignID(key interface{}) uint32 {
 		if len(ip._unusedIDs) != 0 {
 			// Pick an unused id
 			id = ip._unusedIDs[0]
-			ip._unusedIDs = append(ip._unusedIDs[1:])
+			ip._unusedIDs = append(ip._unusedIDs[:0], ip._unusedIDs[1:]...)
 		} else {
 			if len(ip._idsForReuse) != 0 {
 				// Pick one of the ids earlier used for another key
@@ -120,7 +121,7 @@ func deleteRef(refSet []interface{}, ref interface{}) []interface{} {
 	return append(refSet[:i], refSet[i+1:]...)
 }
 
-//  ReleaseID get the reference id
+// ReleaseID get the reference id
 func (ip *IDPool) ReleaseID(key interface{}, ref interface{}) (uint32, uint32) {
 	// with self._lock:
 	log.Printf("IDPool:ReleaseID  Releasing id for key %v", key)
